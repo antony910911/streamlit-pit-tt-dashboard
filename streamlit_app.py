d2085345f9b704a04d1a0e70e858b6c2fa9aad99
antony910911 <antony910912@gmail.com>
2025-06-10 15:12:51 +0800

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,7 +120,7 @@ def fetch_csv_and_load_df(start_date, start_time, end_date, end_time):
 
     timestamp_col = combined_columns[1]
     # 直接轉 naive Asia/Taipei → 不要加 utc=True，不要用 tz_convert → 最穩定
-    df["Datetime"] = pd.to_datetime(df[timestamp_col], unit="s") + pd.Timedelta(hours=8)
+    df["Datetime"] = pd.to_datetime(df[timestamp_col], unit="s") + pd.Timedelta(hours=0)
 
     df.set_index("Datetime", inplace=True)
 
